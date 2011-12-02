c4eecff740c0d6d8e3b4a944e756ad41842fe3da
swazy <patrick.fuerstinode.at>
2011-12-02 23:57:20 +0100

    methods changed

--- src/Light.cpp
+++ src/Light.cpp
@@ -16,29 +16,38 @@ using namespace ci::app;
 
 int Light::amount = 0;
 
-Light::Light( const char* sources){
-    
-    for(int i=0; i < strlen(sources); i++){
-        lightChannels.push_back( LightChannel(sources[i]));
+Light::Light(const string &name){
 
-    }
-       adressOffset = 0;
-    amount++;
-
-    //pos = Vec3i(
-}
-Light::Light(){
-    
-    // default r,g,b
-    lightChannels.push_back( LightChannel('r'));
-    lightChannels.push_back( LightChannel('g'));
-    lightChannels.push_back( LightChannel('b'));
     adressOffset = 0;
     amount++;
-    //pos = Vec3i(
+    this->name = name;
+    this->offset = Vec3i(0,0,0);
+}
+Light::Light(){
+    adressOffset = 0;
+    amount++;
+    this->offset = Vec3i(0,0,0);
+
 }
 
-void Light::setChannelValue(const char* channel, int value){
+void Light::setChannels(vector<LightChannel> &channels){
+    
+    lightChannels = channels;
+
+}
+
+vector<LightChannel>* Light::getChannels(){
+    return &lightChannels;
+}
+
+void Light::addChannel( LightChannel &channel){
+    
+    lightChannels.push_back(channel);
+    
+}
+
+
+void Light::setChannelValue(const char* channel, int value) throw(InvalidValueException){
 
     vector<LightChannel>::iterator it;
     
@@ -50,29 +59,26 @@ void Light::setChannelValue(const char* channel, int value){
             
         }
     }
-    console()<< "Can't set Channel value, because Channel not found.  " << " At Light number " << amount <<endl;
+    throw InvalidValueException( "Can't set Channel value, because Channel not found. At Light " + string(name), value);
 
     
 }
 
 
-int Light::getValueAt(int pos){
+int Light::getValueAt(int pos) throw(InvalidValueException){
     
     if(pos < 0 || pos >= lightChannels.size()){
-       console()<< "Channelposition out of Bounds. Pos: "<<pos  << " At Light number " << amount <<endl;
-    return -1;
+        throw InvalidValueException( "Can't get Value. Channelposition out of Bounds. At Light " + string(name), pos);
     }else
         return lightChannels[pos].getValue();
                  
 }
-const char Light::getChannelAt(int pos){
+char Light::getSourceAt(int pos) throw(InvalidValueException, InvalidSourceException){
     
     if(pos < 0 || pos >= lightChannels.size()){
-        console()<< "Channelposition out of Bounds. Pos: "<<pos  << " At Light  " << "Define Name"<<endl;
-        return 0;
+        throw InvalidValueException( "Can't get Source. Channelposition out of Bounds. At Light " + string(name), pos);
     }else
         return lightChannels[pos].getSource();
-    
     
 }
 int Light::getAmountOfChannels(){
@@ -90,12 +96,23 @@ void Light::setAdressOffset(int o){
 int Light::getAdressOffset(){
     return adressOffset;
 }
-void Light::setPos(Vec3i pos){
+
+string* Light::getName(){
+    return &name;
+}
+
+void Light::setPos(Vec3i &pos){
     
     this->pos = pos;
 }
 
-Vec3i Light::getPos(){
-    return pos;
+Vec3i* Light::getPos(){
+    return &pos;
 }
+
+Vec3i* Light::getOffset(){
+    return &offset;
+}
+
+
 
