e2cd33f3c8b910ec993a536b28c971c06300012b
swazy <patrick.fuerstinode.at>
2011-12-03 17:38:12 +0100

    edited

--- xcode/Resources.h
+++ xcode/Resources.h
@@ -1,4 +1,4 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-#define RES_IMAGE			CINDER_RESOURCE( ../resources/, Rainbow.png, 128, IMAGE )
+#define RES_IMAGE			CINDER_RESOURCE( ../resources/, testbild.png, 128, IMAGE )
